80410ac64fe940fe4c0cc33a6d02acd24fb883f5
Gabriel Bursi <gabriellbursii@gmail.com>
2024-05-11 15:23:26 -0300

    feat: adicionar eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
 		es6: true,
 		node: true,
 		jest: true,
+		"jest/globals": true
 	},
 	extends: [
 		'eslint:recommended',
@@ -13,13 +14,23 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended-requiring-type-checking',
 		'prettier',
 		'plugin:storybook/recommended',
+		'plugin:styled-components-a11y/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:react-native/all',
+		'plugin:react-redux/recommended',
+		"plugin:json/recommended",
+		"plugin:jest-formatting/recommended",
+		"plugin:jest/recommended",
+		"plugin:jest/style",
+		"plugin:react-native/all"
 	],
 	overrides: [
 		{
 			env: {
 				node: true,
 			},
-			files: ['.eslintrc.{js,cjs}'],
+			files: ['.eslintrc.{js,cjs}', '*.spec.tsx'],
+			extends: ['plugin:testing-library/react'],
 			parserOptions: {
 				sourceType: 'script',
 			},
@@ -28,10 +39,26 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		parser: '@typescript-eslint/parser',
-		project: './tsconfig.json',
 		tsconfigRootDir: __dirname,
+		"ecmaFeatures": {
+			"jsx": true
+		}
 	},
-	plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+	plugins: [
+		'react',
+		'react-hooks',
+		'@typescript-eslint',
+		'prettier',
+		'styled-components-a11y',
+		"better-styled-components",
+		"jsx-a11y",
+		"react-native",
+		"react-redux",
+		"json-format",
+		"jest-formatting",
+		'jest',
+		'testing-library'
+	],
 	rules: {
 		'no-empty-function': 'off',
 		'@typescript-eslint/no-empty-function': 'off',
@@ -41,10 +68,24 @@ module.exports = {
 		'react/react-in-jsx-scope': 'off',
 		'react-hooks/exhaustive-deps': 'warn',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		"jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
+		"jest/expect-expect": ["error", { "assertFunctionNames": ["expect"] }],
+		'testing-library/await-async-queries': 'error',
+		'testing-library/no-await-sync-queries': 'error',
+		'testing-library/no-debugging-utils': 'warn',
+		'testing-library/no-dom-import': 'off',
+		"better-styled-components/sort-declarations-alphabetically": 2
 	},
 	settings: {
 		react: {
 			version: 'detect',
 		},
+		"json/sort-package-json": "standard",
+		"json/ignore-files": ["**/package-lock.json"],
+		"json/json-with-comments-files": ["**/tsconfig.json", ".vscode/**"],
 	},
 }
--- jest.config.ts
+++ jest.config.ts
@@ -7,24 +7,12 @@ const config: Config = {
 	setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 	setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
 	modulePaths: ['<rootDir>/src'],
-	transformIgnorePatterns: [
-		'node_modules/(?!@react-native|react-native)'
-	],
+	transformIgnorePatterns: ['node_modules/(?!@react-native|react-native)'],
 	roots: ['<rootDir>/src'],
 	collectCoverageFrom: [
-		'src/components/**/*.tsx',
-		'src/screens/**/*.tsx',
-		'!src/components/**/*.spec.tsx',
-		'!src/components/**/*.stories.tsx',
-		'!src/screens/**/*.spec.tsx',
-		'!src/**/*.spec.tsx',
-		'!src/types/**',
-		'!src/styles/**',
-		'!src/**/*.stories.tsx',
-		'!src/**/types.ts',
-		'!src/**/styles.ts',
-		'!src/components/index.ts',
-		'!src/screens/index.ts',
+		'src/components/**/index.tsx',
+		'src/screens/**/index.tsx',
+		'src/templates/**/index.tsx',
 	],
 }
 
