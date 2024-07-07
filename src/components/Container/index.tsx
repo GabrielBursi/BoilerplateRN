d9c193bb584d441b14c28f92e8a099c689b8dcf5
Gabriel Bursi <gabriellbursii@gmail.com>
2024-07-06 21:27:36 -0300

    feat: adicionar readonly na tipagem das props

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from 'react'
 import * as S from './styles'
 
-export const Container = ({ children }: PropsWithChildren) => {
+export const Container = ({ children }: Readonly<PropsWithChildren>) => {
 	return <S.Container testID="container">{children}</S.Container>
 }
