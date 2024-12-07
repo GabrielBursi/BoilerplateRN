5e96fa0a9c5033d6a501e5ed4693373d3989b569
Gabriel Bursi <gabriellbursii@gmail.com>
2024-12-07 15:54:21 -0300

    refactor: reactotron

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,11 +1,5 @@
 import Reactotron, { networking, openInEditor } from 'reactotron-react-native'
-import mmkvPlugin from 'reactotron-react-native-mmkv'
-import { AsyncStorage } from '@react-native-async-storage/async-storage'
-import { reactotronRedux } from 'reactotron-redux'
 
-import { storage } from './src/storage/MMKStorage'
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 Reactotron.onCustomCommand({
 	command: 'cc',
 	handler: () => Reactotron.log('Custom command'),
@@ -13,21 +7,13 @@ Reactotron.onCustomCommand({
 	description: 'A custom command for example',
 })
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage)
-	.configure({ name: 'React Native Demo' })
-	.useReactNative({
-		devTools: true,
-		asyncStorage: true,
-		storybook: true,
-		networking: true,
-		errors: true,
-		logger: true,
-	})
+const reactotron = Reactotron
+	.configure({ name: 'BolierplateRN' })
+	.useReactNative()
 	.use(networking())
 	.use(openInEditor())
-	.use(reactotronRedux())
-	.use(mmkvPlugin({ storage }))
 	.connect()
+
+console.tron = reactotron;
 
 export default reactotron
